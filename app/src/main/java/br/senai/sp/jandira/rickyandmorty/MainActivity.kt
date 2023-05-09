1eb3961db09515b06ad248f847796ab906a8fe55
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-05-09 11:17:54 -0300

    build: card characters

--- app/src/main/java/br/senai/sp/jandira/rickyandmorty/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/rickyandmorty/MainActivity.kt
@@ -4,25 +4,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.rickyandmorty.model.CharacterList
 import br.senai.sp.jandira.rickyandmorty.model.Info
 import br.senai.sp.jandira.rickyandmorty.service.RetrofitFactory
 import br.senai.sp.jandira.rickyandmorty.ui.theme.RickyAndMortyTheme
+import coil.compose.AsyncImage
 import retrofit2.Callback
 import retrofit2.Call
 import retrofit2.Response
@@ -55,7 +54,7 @@ fun Greeting(name: String) {
         mutableStateOf(Info(0, 0, null, null))
     }
 
-    Column() {
+    Column(modifier = Modifier.padding(start = 32.dp, end = 32.dp)) {
         Button(onClick = {
 
             //Chamada par a API
@@ -83,23 +82,66 @@ fun Greeting(name: String) {
             })
 
 
-        }) {
+        })
+
+        {
+
+
             Text(text = "List all Characters")
         }
         Row() {
-            Text(text = "TOTAL: ", modifier = Modifier.padding(32.dp),fontWeight = FontWeight(800))
-            Text(text = "${info.count}")
+            Text(text = "TOTAL: ",
+                fontWeight = FontWeight(800),
+                color = Color.Blue,
+                modifier = Modifier.size(width = 50.dp, height = 20.dp)
+                )
+            Text(
+                text = "${info.count}",
+                textAlign = TextAlign.End,
+                modifier = Modifier.size(width = 40.dp, height = 20.dp)
+
+            )
         }
         Row() {
-            Text(text = "PAGES: ", fontWeight = FontWeight(800))
-            Text(text = "${info.pages}")
+            Text(
+                text = "PAGES: ",
+                fontWeight = FontWeight(800),
+                color = Color.Blue,
+                modifier = Modifier.size(width = 50.dp, height = 20.dp)
+            )
+            Text(
+                text = "${info.pages}",
+                textAlign = TextAlign.End,
+                modifier = Modifier.size(width = 40.dp, height = 20.dp)
+
+            )
         }
 
         LazyColumn() {
             items(results) {
-                Column() {
-                    Text(text = it.name)
-                    Text(text = it.species)
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                backgroundColor = Color(0xFF4baec6)) {
+                    Row(modifier = Modifier.padding(8.dp)) {
+                        Card(shape = CircleShape) {
+                            AsyncImage(
+                                model = it.image,
+                                contentDescription = "avatar do ${it.name}",
+                                modifier = Modifier
+                                    .size(80.dp)
+
+                            )
+                        }    
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column() {
+                        Text(text = it.name, fontSize = 24.sp, color = Color.White, fontWeight = FontWeight.Bold)
+                        Text(text = it.species, color = Color.White)
+                        Text(text = it.location.name, color = Color.White, fontSize = 12.sp)
+
+                    }
+                    }
                 }
             }
         }
